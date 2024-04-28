f07edcfa3e7a83c091cdedd9877143547cd25c3b
usama387 <128306049+usama387@users.noreply.github.com>
2024-04-28 10:49:23 +0500

    Map integrated in the app Map and pin are child components of Listpage sharing data as props

--- src/routes/ListPage/ListPage.jsx
+++ src/routes/ListPage/ListPage.jsx
@@ -1,25 +1,32 @@
 import Filter from "../../components/Filter/Filter";
 import { listData } from "../../lib/data";
 import Card from "../../components/Card/Card";
+import Location from "../../components/location/locaction";
 import "./ListPage.scss";
 
 const ListPage = () => {
   // dummy data in data.js file to map here
   const data = listData;
 
-  return <div className="listPage">
-    <div className="listContainer">
-      <div className="wrapper">
-        <Filter />
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          {/* filters component */}
+          <Filter />
 
-        {/* Item is the detail of residencies which passed as prop to card to show details */}
-        {data.map((item)=>(
-          <Card key={item.id} item={item}/>
-        ))}
+          {/* Item is the detail of residencies which passed as prop to card to show residence details */}
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        {/* Data is being passed as prop to share locations to map */}
+        <Location items={data} />
       </div>
     </div>
-    <div className="mapContainer">Map</div>
-  </div>;
+  );
 };
 
 export default ListPage;
