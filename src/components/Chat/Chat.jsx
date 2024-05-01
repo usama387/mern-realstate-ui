2b93440e24440fc8827a917efd0c3060cde9d4d2
usama387 <128306049+usama387@users.noreply.github.com>
2024-05-01 13:19:23 +0500

    Specific Chat Window design completed

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./Chat.scss";
 
 const Chat = () => {
+  const [chat, setChat] = useState(true);
   return (
     <div className="chat">
       <div className="messages">
@@ -32,7 +34,54 @@ const Chat = () => {
           <p>Lorem ipsum dolor sit...</p>
         </div>
       </div>
-      <div className="chatBox">Box</div>
+
+      {/* Specific Chat Window when there is chat the div of chatBox will appear */}
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="user"
+              />
+              Usama Razaaq
+            </div>
+            <span className="close" onClick={() => setChat(null)}>
+              X
+            </span>
+          </div>
+          <div className="center">
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit...</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit...</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit...</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit...</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit...</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
